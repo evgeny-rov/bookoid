6d3f8c86fbc8c8eff730ff791c05827b7729e50a
Evgeny Rov <bitsinmyhead@gmail.com>
2020-11-16 15:42:24 +0800

    setting up assets, theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,23 @@
-module.exports = {};
+const brandColors = {
+  primary: '#EBE0DC',
+  'primary-light': '#EEE6E4',
+  secondary: '#5D5D5D',
+  accent: '#FF7797',
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        ...brandColors,
+      },
+      borderRadius: {
+        custom: '1rem',
+      },
+      boxShadow: {
+        'md-reverse':
+          '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
+      },
+    },
+  },
+};
